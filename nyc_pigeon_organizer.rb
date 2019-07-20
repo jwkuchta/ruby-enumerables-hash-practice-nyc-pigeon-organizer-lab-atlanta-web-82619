e94bd2c81eca4dfa15ec0ce579837e6d936d240d
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-20 19:15:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,7 +21,7 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |category, attribute|
-    attribute.each do |attribute, value|
+    attribute.each do |key, value|
       value.each do |name|
         if !pigeon_list.has_key?(name)
           pigeon_list[name] = {}
@@ -29,8 +29,8 @@ def nyc_pigeon_organizer(data)
         if !pigeon_list[name].has_key?(category)
           pigeon_list[name][category] = []
         end
-        if !pigeon_list[name][attribute].include?(value)
-          pigeon_list[name][attribute] = [value.to_s]
+        if !pigeon_list[name][category].include?(value)
+          pigeon_list[name][category] = [value.to_s]
         end
       end
     end
